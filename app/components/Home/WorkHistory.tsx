77aee6434ed53d7e24fe1b642bf03bda49feab21
neelamkhan123 <102263552+neelamkhan123@users.noreply.github.com>
2025-03-25 19:13:12 +0000

    updated employment info

--- app/components/Home/WorkHistory.tsx
+++ app/components/Home/WorkHistory.tsx
@@ -24,7 +24,7 @@ export default function WorkHistory() {
             <h2>Junior Front End Developer</h2>
             <span className="flex items-center space-x-2 bg-white text-slate-900 px-4 py-2 rounded-full shadow-sm text-xs w-fit my-2">
               <Image src={blueBuilding} alt="Company" className="w-3 h-3" />
-              <h4>Euro Communications Ltd</h4>
+              <h4>The Big Phone Store</h4>
             </span>
             <span className="text-[11px]">April 2024 - Present</span>
             <ul className="flex flex-col space-y-2 mt-8 mb-5 ml-4 text-xs md:text-sm list-disc">
@@ -84,7 +84,7 @@ export default function WorkHistory() {
             <h2>Front End Developer Internship</h2>
             <span className="flex items-center space-x-2 bg-white text-slate-900 px-4 py-2 rounded-full shadow-sm text-xs w-fit my-2">
               <Image src={blueBuilding} alt="Company" className="w-3 h-3" />
-              <h4>Euro Communications Ltd</h4>
+              <h4>The Big Phone Store</h4>
             </span>
             <span className="text-[11px]">February 2024 - April 2024</span>
             <ul className="flex flex-col space-y-2 mt-8 mb-5 ml-4 text-xs md:text-sm list-disc">
