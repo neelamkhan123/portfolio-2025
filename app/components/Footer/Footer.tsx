5e6b51d66acb7879c76a6b058bcf77e745dc8472
neelamkhan123 <102263552+neelamkhan123@users.noreply.github.com>
2026-07-31 15:51:11 +0100

    updated CV

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -24,9 +24,9 @@ export default function Footer() {
           <Image src={github} alt="GitHub" className="w-6 h-6" />
         </Link>
         <a
-          href="Neelam Khan - Resume.pdf"
+          href="Neelam Khan - CV.pdf"
           target="_blank"
-          download="Neelam Khan - Resume"
+          download="Neelam Khan - CV"
           className="flex justify-center items-center bg-white w-10 h-10 shadow-2xl rounded-lg cursor-pointer hover:shadow-none hover:scale-105 transition-all duration-300 ease-in-out"
         >
           <Image src={resume} alt="Resume" className="w-5 h-5" />
